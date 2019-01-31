6db92db2d55066af4d9f089c307415a03029fab6
akshay.nambiar <akshay.nambiar@go-jek.com>
2019-01-31 07:52:00 +0530

    | Akshay| Added logic for showing all the leaves for all the users.

--- handler/showleaves.go
+++ handler/showleaves.go
@@ -2,27 +2,39 @@ package handler
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
+
+	"html/template"
 )
+
+type employeeList struct {
+	Employees []employee
+}
+type employee struct {
+	ID     string
+	Name   string
+	Leaves int
+}
 
 func showLeave(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./handler/showleaves.html"))
 		rows, err := db.Query("SELECT * FROM leaves")
 		checkErr(err)
-		var id string
-		var name string
-		var leaves int
+
+		emp := employee{}
+		var emps []employee
 
 		for rows.Next() {
-			err = rows.Scan(&name, &id, &leaves)
-			checkErr(err)
-			fmt.Println(id)
-			fmt.Println(name)
-			fmt.Println(leaves)
+			err = rows.Scan(&emp.Name, &emp.ID, &emp.Leaves)
+			emps = append(emps, emp)
+		}
+
+		data := employeeList{
+			Employees: emps,
 		}
 
 		rows.Close()
-
+		tmpl.Execute(w, data)
 	})
 }
